8044c025a0b2940a55d331a13e0d33068f485356
vasudevanEP <e.p.vasudevan@pointservices.se>
2022-01-19 20:39:05 +0530

    changed the db connection to pdo

--- core/database/DB_Connection.php
+++ core/database/DB_Connection.php
@@ -9,12 +9,24 @@ class DB_Connection{
 	}
 
 
+	// function getConnection(){
+	// 	$conn = new mysqli($this->db_params['servername'],$this->db_params['username'],$this->db_params['password'],$this->db_params['dbname']);
+	// 	if($conn->connect_error){
+	// 		die("Connection Faild: ". $conn->connect_error);
+	// 	}
+	// 	return $conn;
+	// }
 	function getConnection(){
-		$conn = new mysqli($this->db_params['servername'],$this->db_params['username'],$this->db_params['password'],$this->db_params['dbname']);
-		if($conn->connect_error){
-			die("Connection Faild: ". $conn->connect_error);
-		}
-		return $conn;
+		try {
+			$servername = $this->db_params['servername'];
+			$dbname = $this->db_params['dbname'];
+			$conn = new PDO("mysql:host=$servername;dbname=$dbname", $this->db_params['username'], $this->db_params['password']);
+			// set the PDO error mode to exception
+			$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			return $conn;
+		  } catch(PDOException $e) {
+			echo "Connection failed: " . $e->getMessage();
+		  }
 	}
 
 }
